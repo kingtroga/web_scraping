9e61b5e3948333eb4dae85308ebb9801fcfccde1
Tari Yekorogha <trogaclassicman@gmail.com>
2022-07-07 22:40:33 +0100

    Accounting for bad content

--- scrapetest.py
+++ scrapetest.py
@@ -1,23 +1,22 @@
-from urllib.request import urlopen
+# accounting for bad content that is non existant code pages 12 (30/306)
+from urllib.request  import urlopen
 from urllib.error import HTTPError
-from urllib.error import URLError
 from bs4 import BeautifulSoup
 
-try:
-    html = urlopen('http://pythonscraping.com/pages/page1.html')
-except HTTPError as e:
-    print(e)
-except URLError as e:
-    print('The server could not be found!')
+def getTitle(url):
+    try:
+        html = urlopen(url)
+    except HTTPError as e:
+        return None 
+    try:
+        bs = BeautifulSoup(html.read(), 'html.parser')
+        title = bs.body.h1
+    except AttributeError as e:
+        return None
+    return title
+
+title = getTitle('http://www.pythonscraping.com/pages/page1.html')
+if title == None:
+    print('Title could not be found')
 else:
-    print('It Worked!')
-
-
-    
-
-
-
-
-
-bs = BeautifulSoup(html.read(), 'html.parser')
-print(bs.h1)
+    print(title)
