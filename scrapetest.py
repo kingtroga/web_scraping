4c1306084c5dea2d9bbfb70615de66e6acb63af1
Tari Yekorogha <trogaclassicman@gmail.com>
2022-07-08 00:27:27 +0100

    Dealing with parents

--- scrapetest.py
+++ scrapetest.py
@@ -1,4 +1,4 @@
-# Dealing with siblings
+# Dealing with parents
 
 from urllib.request import urlopen
 from bs4 import BeautifulSoup
@@ -7,5 +7,5 @@ from bs4 import BeautifulSoup
 html = urlopen("http://www.pythonscraping.com/pages/page3.html")
 bs = BeautifulSoup(html, 'html.parser')
 
-for child in bs.find('table', {'id':'giftList'}).tr.next_siblings:
-    print(child)
+print(bs.find('img', 
+     b{'src':'../img/gifts/img1.jpg'}).parent.previous_sibling.get_text())
