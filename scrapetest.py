4f6b613adcc689757dab54b71323f5e5c13e69a6
Tari Yekorogha <trogaclassicman@gmail.com>
2022-07-07 22:29:34 +0100

    URLError
    It a server can not be reached at all a URLError must be caught because the remote server is responsible for returning HTTP status codes and since the remote server cannot be found an HTTPError cannot be thrown.

--- scrapetest.py
+++ scrapetest.py
@@ -1,15 +1,19 @@
 from urllib.request import urlopen
 from urllib.error import HTTPError
+from urllib.error import URLError
 from bs4 import BeautifulSoup
 
 try:
     html = urlopen('http://pythonscraping.com/pages/page1.html')
 except HTTPError as e:
     print(e)
-    # return null, break, or do some other "Plan B"
-else: 
-    # Program continues. Note: If you return or break in the
-    # exception catch, you do not need to use the "else" statement
+except URLError as e:
+    print('The server could not be found!')
+else:
+    print('It Worked!')
+
+
+    
 
 
 
