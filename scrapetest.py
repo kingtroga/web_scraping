b24a54933fb83ace19bc29f9f319ec16d6b46c26
Tari Yekorogha <trogaclassicman@gmail.com>
2022-07-08 00:58:03 +0100

    Update scrapetest.py

--- scrapetest.py
+++ scrapetest.py
@@ -8,4 +8,4 @@ html = urlopen("http://www.pythonscraping.com/pages/page3.html")
 bs = BeautifulSoup(html, 'html.parser')
 
 print(bs.find('img', 
-     b{'src':'../img/gifts/img1.jpg'}).parent.previous_sibling.get_text())
+     {'src':'../img/gifts/img1.jpg'}).parent.previous_sibling.get_text())
