21e9af98bfdd7fd69598aa5ef1c8d86698e3499e
Tari Yekorogha <trogaclassicman@gmail.com>
2022-07-08 00:15:38 +0100

    Dealing with children and other descendants.

--- scrapetest.py
+++ scrapetest.py
@@ -1,15 +1,12 @@
-# understanding findAll() and find()
+# Dealing with children and other descendants
+# P.S: All children are descendants, but not all descendats are children.
+
 from urllib.request import urlopen
 from bs4 import BeautifulSoup
 
-html = urlopen('https://www.pythonscraping.com/pages/warandpeace.html')
-bs = BeautifulSoup(html.read(), 'html.parser')
 
+html = urlopen("http://www.pythonscraping.com/pages/page3.html")
+bs = BeautifulSoup(html, 'html.parser')
 
-# find_all(tag, attributes, recursive, text, limit, keywords)
-# find(tag, attributes, recursive, text, keywords)
-
-# Code to find all the number of times "the price" is surrounded by tags
-# on the example page
-nameList = bs.findAll(text='the prince')
-print(len(nameList))
+for child in bs.find('table', {'id':'giftList'}).children:
+    print(child)
