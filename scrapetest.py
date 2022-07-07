3064d363079b8e089e2b1a9d943a4c39f64cd707
Tari Yekorogha <trogaclassicman@gmail.com>
2022-07-07 21:59:47 +0100

    How to handle http errors..

--- scrapetest.py
+++ scrapetest.py
@@ -1,6 +1,19 @@
 from urllib.request import urlopen
+from urllib.error import HTTPError
 from bs4 import BeautifulSoup
 
-html = urlopen('http://pythonscraping.com/pages/page1.html')
+try:
+    html = urlopen('http://pythonscraping.com/pages/page1.html')
+except HTTPError as e:
+    print(e)
+    # return null, break, or do some other "Plan B"
+else: 
+    # Program continues. Note: If you return or break in the
+    # exception catch, you do not need to use the "else" statement
+
+
+
+
+
 bs = BeautifulSoup(html.read(), 'html.parser')
 print(bs.h1)
