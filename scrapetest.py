af6a0fef9baeca5641346dd72d2bd5b0fc176bde
Tari Yekorogha <trogaclassicman@gmail.com>
2022-07-08 01:56:19 +0100

    RegularExpressions And BeautifulSoap

--- scrapetest.py
+++ scrapetest.py
@@ -1,11 +1,13 @@
-# Dealing with parents
+# RegularExpressions and BeautifulSoap
 
 from urllib.request import urlopen
 from bs4 import BeautifulSoup
+import re
 
-
-html = urlopen("http://www.pythonscraping.com/pages/page3.html")
+html = urlopen("https://www.pythonscraping.com/pages/page3.html")
 bs = BeautifulSoup(html, 'html.parser')
-
-print(bs.find('img', 
-     {'src':'../img/gifts/img1.jpg'}).parent.previous_sibling.get_text())
+images = bs.find_all("img", 
+    {'src': re.compile('\.\.\/img\/gifts/img.*\.jpg')}
+)
+for image in images:
+    print(image['src'])
