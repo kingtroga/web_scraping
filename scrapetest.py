504f69633ac1e1dc11494d9d91f0ceab8855d139
Tari Yekorogha <trogaclassicman@gmail.com>
2022-07-14 19:13:32 +0100

    Python has a recursion limit

--- scrapetest.py
+++ scrapetest.py
@@ -1,19 +1,20 @@
-# Six degrees of Wikipedia
+# Crawling an entire webpage
 from urllib.request import urlopen
 from bs4 import BeautifulSoup
-import datetime
-import random
 import re
 
-random.seed(datetime.datetime.now())
-def getLinks(articleUrl):
-    html = urlopen('http://en.wikipedia.org{}'.format(articleUrl))
+pages = set()
+def getLinks(pageUrl):
+    global pages
+    html = urlopen(f'http://en.wikipedia.org{pageUrl}')
     bs = BeautifulSoup(html, 'html.parser')
-    return bs.find('div', {'id': 'bodyContent'}).find_all('a',
-        href=re.compile('^(/wiki/)((?!:).)*$'))
+    for link in bs.find_all('a', href=re.compile('^(/wiki/)')):
+        if 'href' in link.attrs:
+            if link.attrs['href'] not in pages:
+                #We have encountered a new page
+                newPage = link.attrs['href']
+                print(newPage)
+                pages.add(newPage)
+                getLinks(newPage)
 
-links = getLinks('/wiki/Kevin_Bacon')
-while len(links) > 0:
-    newArticle = links[random.randint(0, len(links)-1)].attrs['href']
-    print(newArticle)
-    links = getLinks(newArticle)
+getLinks('')
