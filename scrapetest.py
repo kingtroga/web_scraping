48eef265e76305a0fd3758403a953a5770da28ad
Tari Yekorogha <trogaclassicman@gmail.com>
2022-07-14 18:48:04 +0100

    six degrees of wikipedia

--- scrapetest.py
+++ scrapetest.py
@@ -1,19 +1,19 @@
-# Writing Web Crawlers
-# a Python script that retrieves an arbitrary
-# Wikipedia page and produces a list of links on that page
-
-
+# Six degrees of Wikipedia
 from urllib.request import urlopen
 from bs4 import BeautifulSoup
+import datetime
+import random
 import re
 
-# added the three tricks from the book
-html = urlopen('http://en.wikipedia.org/wiki/Kevin_Bacon')
-bs = BeautifulSoup(html, 'html.parser')
+random.seed(datetime.datetime.now())
+def getLinks(articleUrl):
+    html = urlopen('http://en.wikipedia.org{}'.format(articleUrl))
+    bs = BeautifulSoup(html, 'html.parser')
+    return bs.find('div', {'id': 'bodyContent'}).find_all('a',
+        href=re.compile('^(/wiki/)((?!:).)*$'))
 
-
-for link in bs.find('div', {'id': 'bodyContent'}).find_all(
-    'a', href=re.compile('^(/wiki/)((?!:).)*$')):
-    if 'href' in link.attrs:
-        print(link.attrs['href'])
-
+links = getLinks('/wiki/Kevin_Bacon')
+while len(links) > 0:
+    newArticle = links[random.randint(0, len(links)-1)].attr['href']
+    print(newArticle)
+    links = getLinks(newArticle)
