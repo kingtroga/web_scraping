c4923356d7a0682ca2c99cb671182791f80534c9
Tari Yekorogha <trogaclassicman@gmail.com>
2022-07-15 11:51:26 +0100

    found errors

--- wikiSpider/wikiSpider/spiders/articles.py
+++ wikiSpider/wikiSpider/spiders/articles.py
@@ -6,18 +6,14 @@ class ArticleSpider(CrawlSpider):
     allowed_domains = ['wikipedia.org']
     start_urls = ['https://en.wikipedia.org/wiki/Benevolent_dictator_for_life']
     rules = [Rule(LinkExtractor(allow=r'.*'), callback='parse_items', follow=True)]
-    
+
     def parse_items(self, response):
         url = response.url
         title = response.css('h1::text').extract_first()
         text = response.xpath('//div[@id="mw-content-text"]//text()').extract()
         lastUpdated = response.css('li#footer-info-lastmod::text').extract_first()
-        lastUpdated = lastUpdated.replace(
-            'This page was last edited on ', ''
-        )
+        lastUpdated = lastUpdated.replace('This page was last edited on ', '')
         print('URL is: {}'.format(url))
-        print('title is {} '. format(title))
+        print('title is: {} '.format(title))
         print('text is: {}'.format(text))
         print('Last updated: {}'.format(lastUpdated))
-
-        
